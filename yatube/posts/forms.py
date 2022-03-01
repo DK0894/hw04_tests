7af40309d934ed956ba48d5fae54fc6fa03fa384
Denis Kudakov <denn88kudakov@yandex.ru>
2022-03-01 19:38:17 +0300

    Add custom error pages

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,12 +7,11 @@ from .validators import clean_text
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('text', 'group', 'image')
+        fields = ('text', 'group')
         widgets = {'text': forms.Textarea}
         labels = {
             'text': 'Введите текст нового поста',
             'group': 'Выберите группу',
-            'image': 'Загрузите картинку',
         }
         help_texts = {'group': 'К этой группе будет относиться ваш пост'}
         validators = {'text': clean_text}
