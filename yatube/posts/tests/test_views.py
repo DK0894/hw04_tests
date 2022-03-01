1c315872c52e4e37ca66f56714ba871d083a4ee2
Denis Kudakov <denn88kudakov@yandex.ru>
2022-03-01 19:49:45 +0300

    Исправлены замечания по проекту спринта 5, добывлена возможнасть загружать картинки к постам, добавлены кастомные страницы ошибок.

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
